93e6ad9c56a64bd14f16ab0e733f748d19abb424
karthik-karalgikar <karalgikarkarthik@gmail.com>
2024-05-04 00:05:23 +0530

    text, icon, button, icon as a button

--- lib/main.dart
+++ lib/main.dart
@@ -9,25 +9,53 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text('my first app'),
+          title: Text('Weather App'),
           centerTitle: true,
           backgroundColor: Colors.blue
       ),
-      body: const Center(
-        child: Text(
-          'hello lalalala!',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.amber,
-            fontFamily: 'IndieFlower',
+      body: Center(
+        //text
+
+        // child: Text(
+        //   'hello lalalala!',
+        //   style: TextStyle(
+        //     fontSize: 20.0,
+        //     fontWeight: FontWeight.bold,
+        //     letterSpacing: 2.0,
+        //     color: Colors.amber,
+        //     fontFamily: 'IndieFlower',
+        //   ),
+        // ),
+
+        //image
+        // child: Image.asset('assets/test1.jpeg'),
+
+        //button
+        // child: ElevatedButton.icon(
+        //     onPressed: (){},
+        //     icon: Icon(
+        //         Icons.airline_seat_flat_angled
+        //     ),
+        //     label: Text('omomom'),
+        //   style: ElevatedButton.styleFrom(
+        //     backgroundColor: Colors.amber
+        //   ),
+        // ),
+
+        //Icon as a button
+        child: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.email
           ),
+          color: Colors.amber,
         ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.blue,
-        onPressed: () {  },
+        onPressed: () {
+          print('kakakaka');
+        },
         child: Text('click'),
       ),
     );
