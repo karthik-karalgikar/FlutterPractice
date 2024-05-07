da419742cb7e372c418a864a083f0caf3f87131c
karthik-karalgikar <karalgikarkarthik@gmail.com>
2024-05-08 01:02:50 +0530

    row

--- lib/main.dart
+++ lib/main.dart
@@ -13,49 +13,25 @@ class Home extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.blue
       ),
-      body: Container(
-        // padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-        // padding: EdgeInsets.all(20.0),
-        padding: EdgeInsets.fromLTRB(10.0, 100.0, 10.0, 10.0),
-        margin: EdgeInsets.all(150.0),
-        color: Colors.grey[400],
-        child: Text('hello'),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Hello World'),
+          TextButton(
+              onPressed: (){},
+              child: Text('Click me here'),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.amber)
+          ),
+          ),
+          Container(
+            color: Colors.cyan,
+            padding: EdgeInsets.all(50.0),
+            child: Text('Hello lalala'),
+          )
+        ],
         //text
-
-        // child: Text(
-        //   'hello lalalala!',
-        //   style: TextStyle(
-        //     fontSize: 20.0,
-        //     fontWeight: FontWeight.bold,
-        //     letterSpacing: 2.0,
-        //     color: Colors.amber,
-        //     fontFamily: 'IndieFlower',
-        //   ),
-        // ),
-
-        //image
-        // child: Image.asset('assets/test1.jpeg'),
-
-        //button
-        // child: ElevatedButton.icon(
-        //     onPressed: (){},
-        //     icon: Icon(
-        //         Icons.airline_seat_flat_angled
-        //     ),
-        //     label: Text('omomom'),
-        //   style: ElevatedButton.styleFrom(
-        //     backgroundColor: Colors.amber
-        //   ),
-        // ),
-
-        //Icon as a button
-        // child: IconButton(
-        //   onPressed: () {},
-        //   icon: Icon(
-        //     Icons.email
-        //   ),
-        //   color: Colors.amber,
-        // ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.blue,
